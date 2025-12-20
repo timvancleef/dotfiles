7d8b7d414d85396802e03a06200dc2d8264e19f4
Tim van Cleef <Tim van Cleeftvc+github@posteo.de>
2025-12-20 10:29:04 +0100

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,11 +17,11 @@ vim.opt.smarttab = true
 --
 -- UI config
 --
-vim.opt.number = true         -- show absolute number
+vim.opt.number = true     -- show absolute number
 -- vim.opt.relativenumber = true -- add numbers to each line on the left side
-vim.opt.cursorline = true     -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true     -- open new vertical split bottom
-vim.opt.splitright = true     -- open new horizontal splits right
+vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true -- open new vertical split bottom
+vim.opt.splitright = true -- open new horizontal splits right
 
 --
 -- Searching
@@ -109,10 +109,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 vim.keymap.set("n", "N", "Nzzzv", opts)
-vim.keymap.set("n", "N", "Nzzzv", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
 
 -- Paste and delete into void (do not replace yank buffer)
-vim.keymap.set("x", "<leader>p", [["_dP]], opts)
+vim.keymap.set({ "x", "v" }, "<leader>p", [["_dP]], opts)
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], opts)
 
 -- Yank into system clipboard
@@ -131,9 +131,10 @@ vim.keymap.set("n", "<C-n>", ":NnnPicker %:p:h<CR>", opts)
 
 -- Close quickfix menu after selecting choice
 vim.api.nvim_create_autocmd(
-  "FileType", {
-  pattern={"qf"},
-  command=[[nnoremap <buffer> <CR> <CR>:cclose<CR>]]})
+    "FileType", {
+        pattern = { "qf" },
+        command = [[nnoremap <buffer> <CR> <CR>:cclose<CR>]]
+    })
 
 vim.keymap.set("n", "<leader>co", ":copen<CR>", opts)
 
@@ -250,17 +251,32 @@ end
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
+    callback = function(event)
+        local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
         -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
+        local opts = { buffer = event.buf }
         -- vim.keymap.set('i', '<c-space>', vim.lsp.omnifunc, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+        -- Jump to the definition of the word under your cursor.
+        --  This is where a variable was first declared, or where a function is defined, etc.
+        --  To jump back, press <C-t>.
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        -- Jump to the implementation of the word under your cursor.
+        --  Useful when your language has ways of declaring types without an actual implementation.
+        map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+        -- Find references for the word under your cursor.
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
         vim.keymap.set('n', 'gf', function()
             vim.lsp.buf.format { async = true }
         end, opts)
@@ -271,6 +287,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<leader>cp', vim.diagnostic.goto_prev, opts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 
+        map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+        map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+
         -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
         -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
         -- vim.keymap.set('n', '<space>wl', function()
@@ -280,6 +299,35 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
+-- Diagnostic Config
+-- See :help vim.diagnostic.Opts
+vim.diagnostic.config {
+    severity_sort = true,
+    float = { border = 'rounded', source = 'if_many' },
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    signs = vim.g.have_nerd_font and {
+        text = {
+            [vim.diagnostic.severity.ERROR] = 'e',
+            [vim.diagnostic.severity.WARN] = 'w',
+            [vim.diagnostic.severity.INFO] = 'i',
+            [vim.diagnostic.severity.HINT] = 'h',
+        },
+    } or {},
+    virtual_text = {
+        source = 'if_many',
+        spacing = 2,
+        format = function(diagnostic)
+            local diagnostic_message = {
+                [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                [vim.diagnostic.severity.WARN] = diagnostic.message,
+                [vim.diagnostic.severity.INFO] = diagnostic.message,
+                [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+        end,
+    },
+}
+
 -- nvim-cmp setup
 local cmp = require('cmp')
 cmp.setup {
@@ -288,25 +336,24 @@ cmp.setup {
         -- ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
         -- C-b (back) C-f (forward) for snippet placeholder navigation.
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
+        ['<C-y'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ['<C-n>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             else
                 fallback()
             end
         end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                else
-                    fallback()
-                end
-            end,
-            { 'i', 's' }),
+        ['<C-p>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
     }),
     sources = {
         { name = 'nvim_lsp', keyword_length = 3, max_item_count = 5 },
